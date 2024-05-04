1e485a22dc0a6ebf81e52014398c46c0d859134a
ulmas9025 <i.ulmas9025@gmail.com>
2024-05-04 18:41:57 +0500

    first commit

--- express-js/lesson03_forms_getAndPost/home-work/app.mjs
+++ express-js/lesson03_forms_getAndPost/home-work/app.mjs
@@ -1,36 +1,45 @@
 import express from 'express';
-import fs from 'fs';
-import path from 'path';
-import crClient from "./crClent.mjs";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = 5555;
 
-
-
-app.get("/", (req, res) => {
-    res.send(200);
-  });
-  
-  app.use("/public", crClient);
-
-fs.readFile('./client.json', 'utf-8',(err, jsonString)=>{
-  if(err){
-    console.log(err);
-  }else{
-    try{
-      const data =JSON.parse(jsonString)
-      console.log(data);
-    }catch{
-      console.log('Error parsing JSON' , err);
-    }
-  }
-})
-
-
-
-
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'registor.html'));
+});
 
 app.listen(PORT, () => {
-    console.log(`Server http://localhost:${PORT} portida ishga tushdi`);
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get('/reg', (req, res) => {
+  res.sendFile(`${__dirname}/public/registor.html`);
+});
+
+app.get('/start', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/registor.html`);
+});
+
+app.post('/register', (req, res) => {
+  // Получаем данные из запроса
+  const { name, surname, email, password } = req.body;
+
+  // Создаем объект с данными о пользователе
+  const clientUser = {
+    name: name,
+    surname: surname,
+    email: email,
+    password: password
+  };
+
+  // Отправляем ответ после получения данных из тела запроса
+  res.send('POST request to the register page');
 });
